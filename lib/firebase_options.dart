368cbb0a0a1b5d8182b27e03d80cf2a6bbc9444d
migarakavishan <migarakavishan43@gmail.com>
2024-06-21 15:03:04 +0530

    add .env for api keys

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -40,49 +41,49 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyBhNiDIsoSIN57PNupE2smjG1M_mXIDHXs',
-    appId: '1:741746818037:web:a2b19e37824b911c4c7ebc',
-    messagingSenderId: '741746818037',
-    projectId: 'apple---ecommerce',
-    authDomain: 'apple---ecommerce.firebaseapp.com',
-    storageBucket: 'apple---ecommerce.appspot.com',
-    measurementId: 'G-TR51KZ2D9V',
+  static FirebaseOptions web = FirebaseOptions(
+    apiKey: dotenv.env['WEB_API_KEY']!,
+    appId: dotenv.env['WEB_APP_ID']!,
+    messagingSenderId: dotenv.env['WEB_MESSAGING_SENDER_ID']!,
+    projectId: dotenv.env['WEB_PROJECT_ID']!,
+    authDomain: dotenv.env['WEB_AUTH_DOMAIN']!,
+    storageBucket: dotenv.env['WEB_STORAGE_BUCKET']!,
+    measurementId: dotenv.env['WEB_MEASUREMENT_ID'],
   );
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyCKYz10sL__LdXamohtnIBvCgcBu6rliMo',
-    appId: '1:741746818037:android:bd3c648c75a696fa4c7ebc',
-    messagingSenderId: '741746818037',
-    projectId: 'apple---ecommerce',
-    storageBucket: 'apple---ecommerce.appspot.com',
+  static FirebaseOptions android = FirebaseOptions(
+    apiKey: dotenv.env['ANDROID_API_KEY']!,
+    appId: dotenv.env['ANDROID_APP_ID']!,
+    messagingSenderId: dotenv.env['ANDROID_MESSAGING_SENDER_ID']!,
+    projectId: dotenv.env['ANDROID_PROJECT_ID']!,
+    storageBucket: dotenv.env['ANDROID_STORAGE_BUCKET']!,
   );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyAsmsyErUNuKF5R6gU2ukvZWYLx2htdiqs',
-    appId: '1:741746818037:ios:616a6bd100c78cba4c7ebc',
-    messagingSenderId: '741746818037',
-    projectId: 'apple---ecommerce',
-    storageBucket: 'apple---ecommerce.appspot.com',
-    iosBundleId: 'lk.migara.apple',
+  static FirebaseOptions ios = FirebaseOptions(
+    apiKey: dotenv.env['IOS_API_KEY']!,
+    appId: dotenv.env['IOS_APP_ID']!,
+    messagingSenderId: dotenv.env['IOS_MESSAGING_SENDER_ID']!,
+    projectId: dotenv.env['IOS_PROJECT_ID']!,
+    storageBucket: dotenv.env['IOS_STORAGE_BUCKET']!,
+    iosBundleId: dotenv.env['IOS_BUNDLE_ID']!,
   );
 
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyAsmsyErUNuKF5R6gU2ukvZWYLx2htdiqs',
-    appId: '1:741746818037:ios:716dda44b9d1a0784c7ebc',
-    messagingSenderId: '741746818037',
-    projectId: 'apple---ecommerce',
-    storageBucket: 'apple---ecommerce.appspot.com',
-    iosBundleId: 'com.example.appleEcommerce',
+  static FirebaseOptions macos = FirebaseOptions(
+    apiKey: dotenv.env['MACOS_API_KEY']!,
+    appId: dotenv.env['MACOS_APP_ID']!,
+    messagingSenderId: dotenv.env['MACOS_MESSAGING_SENDER_ID']!,
+    projectId: dotenv.env['MACOS_PROJECT_ID']!,
+    storageBucket: dotenv.env['MACOS_STORAGE_BUCKET']!,
+    iosBundleId: dotenv.env['MACOS_BUNDLE_ID']!,
   );
 
-  static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyBhNiDIsoSIN57PNupE2smjG1M_mXIDHXs',
-    appId: '1:741746818037:web:30bf4471797585754c7ebc',
-    messagingSenderId: '741746818037',
-    projectId: 'apple---ecommerce',
-    authDomain: 'apple---ecommerce.firebaseapp.com',
-    storageBucket: 'apple---ecommerce.appspot.com',
-    measurementId: 'G-R45RX511NE',
+  static FirebaseOptions windows = FirebaseOptions(
+    apiKey: dotenv.env['WINDOWS_API_KEY']!,
+    appId: dotenv.env['WINDOWS_APP_ID']!,
+    messagingSenderId: dotenv.env['WINDOWS_MESSAGING_SENDER_ID']!,
+    projectId: dotenv.env['WINDOWS_PROJECT_ID']!,
+    authDomain: dotenv.env['WINDOWS_AUTH_DOMAIN']!,
+    storageBucket: dotenv.env['WINDOWS_STORAGE_BUCKET']!,
+    measurementId: dotenv.env['WINDOWS_MEASUREMENT_ID'],
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,14 @@ import 'package:apple_ecommerce/providers/user_provider.dart';
 import 'package:apple_ecommerce/screens/auth_screens/auth_state_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:provider/provider.dart';
 
 import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
